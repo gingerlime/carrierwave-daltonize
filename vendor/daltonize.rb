536bad5cfc850c66ba34a8591782d698bf03c878
Yoav <yoav@gingerlime.com>
2013-02-18 19:34:42 +0000

    merging @jcupitt's version in daltonize.rb
    * daltonize.rb can be run standalone
    * it is also launched from carrierwave

--- vendor/daltonize.rb
+++ vendor/daltonize.rb
@@ -2,27 +2,46 @@ require 'rubygems'
 require 'vips'
 
 def deuteranope
-  # we need rows to sum to 1 in Bradford space, so the matrix was normalized
-  # (each value divided by the sum of the values in a row)
-  [[1, 0, 0],
-   [0.2836, 0.0, 0.7164],
-   [0, 0, 1]]
+  # deuteranopes are missing green receptors, so to simulate their vision 
+  # we replace the green signal with a 70/30 mix of red and blue
+  # to correct, we put 50% of the red/green error into lightness and 100%
+  # into yellow/blue
+  [[[  1,   0,   0],
+    [0.7,   0, 0.3],
+    [  0,   0,   1]],
+   [[  1, 0.5,   0],
+    [  0,   0,   0],
+    [  0,   1,   1]]]
 end
 
 def protanope
-  [[0, 4.0277, -5.0277],
-   [0, 1, 0],
-   [0, 0, 1]]
+  # protanopes are missing red receptors --- we simulate their condition by
+  # replacing the red signal with an 80/20 mix of green and blue (since 
+  # blue is far less important than green)
+  # correction as for deuts
+  [[[  0, 0.8, 0.2],
+    [  0,   1,   0],
+    [  0,   0,   1]],
+   [[  1, 0.5,   0],
+    [  0,   0,   0],
+    [  0,   1,   1]]]
 end
 
 def tritanope
-  [[1, 0, 0],
-   [0, 1, 0],
-   [-0.977, 1.977, 0.0]]
+  # tritanopes are missing blue receptors --- we replace the blue signal
+  # with 30/70 red/green
+  # to correct, we put 50% of the yellow/blue error into lightness, and 
+  # 100% into red/green
+  [[[  1,   0,   0],
+    [  0,   1,   0],
+    [0.3, 0.7,   0]], 
+   [[  1,   0, 0.5],
+    [  0,   0,   1],
+    [  0,   0,   0]]]
 end
 
-def get_matrix (prefix)
-  matrix = case prefix
+def get_matrices (prefix)
+  matrices = case prefix
     when "d" then deuteranope
     when "p" then protanope
     when "t" then tritanope
@@ -30,7 +49,7 @@ def get_matrix (prefix)
   end
 end
 
-def daltonize (image, matrix)
+def daltonize (image, simulate, distribute)
 
   # remove any alpha channel before processing
   alpha = nil
@@ -40,50 +59,40 @@ def daltonize (image, matrix)
   end
 
   begin
-      # import to CIELAB with lcms
-      # if there's no profile there, we'll fall back to the thing below
-      lab = image.icc_import_embedded(:relative)
-      xyz = lab.lab_to_xyz()
+    # import to CIELAB with lcms
+    # if there's no profile there, we'll fall back to the thing below
+    cielab = image.icc_import_embedded(:relative)
+    xyz = lab.lab_to_xyz()
   rescue VIPS::Error
-      # nope .. use the built-in converter instead
-      xyz = image.srgb_to_xyz()
+    # nope .. use the built-in converter instead
+    xyz = image.srgb_to_xyz()
+    cielab = xyz.xyz_to_lab()
   end
 
-  # and now to bradford cone space (a variant of LMS)
+  # to bradford cone space (a variant of LMS)
   brad = xyz.recomb([[0.8951,  0.2664, -0.1614],
                      [-0.7502,  1.7135,  0.0367],
                      [0.0389, -0.0685,  1.0296]])
 
-  # through the provided daltonize matrix
-  mat = brad.recomb(matrix)
+  # through the color-vision deficit matrix
+  simu = brad.recomb(simulate)
 
   # back to xyz (this is the inverse of the brad matrix above)
-  xyz = mat.recomb([[0.987, -0.147, 0.16],
-                     [0.432, 0.5184, 0.0493],
-                     [-0.0085, 0.04, 0.968]])
-  # and now to bradford cone space (a variant of LMS)
-  brad = xyz.recomb([[0.8951,  0.2664, -0.1614],
-                     [-0.7502,  1.7135,  0.0367],
-                     [0.0389, -0.0685,  1.0296]])
-
-  # through the provided daltonize matrix
-  mat = brad.recomb(matrix)
-
-  # back to xyz (this is the inverse of the brad matrix above)
-  xyz = mat.recomb([[0.987, -0.147, 0.16],
+  xyz2 = simu.recomb([[0.987, -0.147, 0.16],
                      [0.432, 0.5184, 0.0493],
                      [-0.0085, 0.04, 0.968]])
 
-  # .. and back to sRGB
-  rgb = xyz.xyz_to_srgb()
 
-  # so this is the colour error
-  err = image - rgb
+  # now find the error in CIELAB
+  cielab2 = xyz2.xyz_to_lab()
+  err = cielab - cielab2
 
-  # add the error back to other channels to make a compensated image
-  image = image + err.recomb([[0, 0, 0],
-                              [0.7, 1, 0],
-                              [0.7, 0, 1]])
+  # add the error channels back to the original, recombined so as to hit
+  # channels the person is sensitive to
+  cielab = cielab + err.recomb(distribute)
+
+  # .. and back to sRGB 
+  image = cielab.lab_to_xyz().xyz_to_srgb()
 
   # reattach any alpha we saved above
   if alpha
@@ -100,13 +109,13 @@ if __FILE__ == $0
     exit 1
   end
 
-  matrix = get_matrix(ARGV[2])
-  if matrix.nil?
+  matrices = get_matrices(ARGV[2])
+  if matrices.nil?
     puts "color-deficiency must be either d,t or p"
     exit 1
   end
 
   image = VIPS::Image.new(ARGV[0])
-  image = daltonize(image, matrix)
+  image = daltonize(image, *matrices)
   image.write(ARGV[1])
 end
